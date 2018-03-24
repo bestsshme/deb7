03f561e8f043bf0f0d07d4a896631c54efa86844
bestsshme <35902661+bestsshme@users.noreply.github.com>
2018-03-24 22:16:07 +0700

    Update jucky.sh

--- jucky.sh
+++ jucky.sh
@@ -52,6 +52,45 @@ UPDATE AND UPGRADE PROCESS
 
 PLEASE WAIT TAKE TIME 1-5 MINUTE
 "
+#detail nama perusahaan
+country=ID
+state=JAGOANSSH.COM
+locality=JAGOANSSH.COM
+organization=JAGOANSSH.COM
+organizationalunit=ID
+commonname=JAGOANSSH.COM
+email=ADMIN@JAGOANSSH.COM
+
+# go to root
+cd
+
+# install stunnel
+apt-get install stunnel4 -y
+cat > /etc/stunnel/stunnel.conf <<-END
+cert = /etc/stunnel/stunnel.pem
+client = no
+socket = a:SO_REUSEADDR=1
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+
+
+[dropbear]
+accept = 443
+connect = 127.0.0.1:3128
+
+END
+
+#membuat sertifikat
+openssl genrsa -out key.pem 2048
+openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
+-subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
+
+#konfigurasi stunnel
+sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+/etc/init.d/stunnel4 restart
+
+
 # install essential package
 apt-get -y install build-essential
 apt-get -y install cron bmon iftop htop nmap axel nano iptables traceroute sysv-rc-conf dnsutils bc nethogs openvpn vnstat less screen psmisc apt-file whois ptunnel ngrep mtr git zsh mrtg snmp snmpd snmp-mibs-downloader unzip tar zip rsyslog debsums rkhunter
